6bc9f442f3518c1162eb947f6dc7d28b7f0dd2e6
Jari Aalto <jari.aalto@cante.net>
2011-04-08 17:36:26 +0300

    patches/debian: 0.48-7 add
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ patches/debian/debian-patch-compile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This file compiles debian patched from DIR (downloaded there)
+# The debian version is announced in stamp file *.debian
+
+NAME=quilt
+VER=_*[0-9]*-[0-9]*
+EXT=.debian
+DIR=patches.tmp
+
+
+Main ()
+{
+    file=$( ls $NAME$VER$ext | tail --lines=1 )
+
+    [ "$file" ] || return 1
+
+    ver=$( echo $file | sed -e "s,$NAME[_-],," -e s",$EXT,," )
+    PATCH=$NAME-$ver-debian-patches.patch
+
+    [ "$ver" ] || return 2
+
+    rm -f $PATCH
+
+    while read item
+    do
+	cat $DIR/$item >> $PATCH
+	
+    done < <(grep -vE '#' $DIR/series)
+
+    lsdiff $PATCH
+    echo "# Compiled: $PATCH"
+}
+
+Main "$@"
+
+# End of file
